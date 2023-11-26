cd4456c95fdf284a7167bd852531f1780b69f737
algorusty <3823121+algorusty@users.noreply.github.com>
2023-11-26 17:57:27 -0600

    random bg movement, more frostyblur

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import {
     Navbar, Home, About, Projects, Skills, Contact
 } from './components/Components';
 import './components/Components.css';
-// import Navbar from './components/Navbar';
-// import Home from './components/Home';
-// import About from './components/About';
-// import Projects from './components/Projects';
-// import Skills from './components/Skills';
-// import Contact from './components/Contact';
 
 function App() {
+    useEffect(() => {
+        // Function to select a random background
+        const selectRandomBackground = () => {
+            const backgrounds = ['background-hard.png', 'background-soft.png', 'background.png'];
+            const randomIndex = Math.floor(Math.random() * backgrounds.length);
+            const randomBackground = backgrounds[randomIndex];
+            // Set the CSS variable to the selected background
+            document.documentElement.style.setProperty('--random-background', `url('../../public/${randomBackground}')`);
+        };
+
+        selectRandomBackground();
+
+        // Function to generate a random value between -10px and 10px
+        function getRandomPosition() {
+            return Math.floor(Math.random() * 21) - 30 + 'px';
+        }
+
+        // Function to update the background position
+        function updateBackgroundPosition() {
+            const x = `calc(50% + ${getRandomPosition()})`;
+            const y = `calc(50% + ${getRandomPosition()})`;
+            document.body.style.backgroundPosition = `${x} ${y}`;
+        }
+
+        // Update the background position every 5 seconds
+        const intervalId = setInterval(updateBackgroundPosition, 5000);
+
+        // Clean up function
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
     return (
         <div className="App">
             <Navbar />
@@ -20,7 +47,6 @@ function App() {
             <About />
             <Projects />
             <Skills />
-            
         </div>
     );
 }
